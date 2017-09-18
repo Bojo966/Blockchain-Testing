81ecae1f2f7dfc19e9ea04d6ec91391ab7a116d4
Bojo <stil.2910@gmail.com>
2017-09-18 18:00:03 +0300

    Updating the repo with the final finales for foodSafe

--- WebpackSample/contracts/FoodSafe.sol
+++ WebpackSample/contracts/FoodSafe.sol
@@ -1,19 +1,36 @@
-pragma solidity ^0.4.6;
+pragma solidity ^ 0.4.6;
 
 contract FoodSafe {
-    
+
     struct Location {
         string Name;
         uint LocationId;
         uint PreviousLocationId;
-        uint Timstamp;
+        uint Timestamp;
         string Secret;
     }
 
-    mapping (uint=>Location) Trail;
-    uint8 TrailCount=0;
+    mapping(uint => Location) Trail;
+    uint8 TrailCount = 0;
 
     function AddNewLocation(uint LocationId, string Name, string Secret) {
-        
+        Location memory newLocation;
+        newLocation.Name = Name;
+        newLocation.LocationId = LocationId;
+        newLocation.Secret = Secret;
+        newLocation.Timestamp = now;
+
+        if (TrailCount != 0) {
+            newLocation.PreviousLocationId = Trail[TrailCount].LocationId;
+            TrailCount++;
+        }
+    }
+
+    function GetTrailCount() returns(uint8) {
+        return TrailCount;
+    }
+
+    function GetLocation(uint8 TrailNo) returns(string, uint, uint,uint,string) {
+        return (Trail[TrailNo].Name, Trail[TrailNo].LocationId, Trail[TrailNo].PreviousLocationId, Trail[TrailNo].Timestamp, Trail[TrailNo].Secret);
     }
 }
